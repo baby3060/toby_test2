8c20d1355e37d99460d5cf2d52526db8bfa148e8
psk <psk3060@naver.com>
2019-04-04 18:47:23 +0900

    Level Upgrade 대상여부 판별 User

--- src/main/java/com/tobsec/model/User.java
+++ src/main/java/com/tobsec/model/User.java
@@ -8,6 +8,9 @@ import lombok.*;
 // equals와 hashCode 구현 시 level, login, recommend 필드 제외하고 구현
 @EqualsAndHashCode(exclude = {"level", "login", "recommend"})
 public class User {
+    private final int LOGIN_MIN_SILVER = 50;
+    private final int RECOMMEND_MIN_GOLD = 30;
+
     private String id;
     private String name;
     private String password;
@@ -38,4 +41,23 @@ public class User {
             this.setLevel(nextLevel);
         }
     }
+
+    /**
+     * User의 등급 상승 대상인지?
+     */
+    public boolean isLvlUpTarget() {
+        boolean isSilver = (login >= 50);
+
+        switch(this.level) {
+            case BRONZE : 
+                return isSilver;
+            case SILVER : 
+                return isSilver && recommend >= RECOMMEND_MIN_GOLD;
+            case GOLD:
+                return false;
+            default:
+                throw new IllegalStateException("잘못된 등급입니다.");
+        }
+    }
+
 }
