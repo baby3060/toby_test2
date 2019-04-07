e131008b53674b39f4533e90a290e6b07a14f950
psk <psk3060@naver.com>
2019-04-07 12:37:05 +0900

    Logger 애노테이션 사용 위한 사전 작업

--- src/main/java/com/tobsec/common/Log.java
+++ src/main/java/com/tobsec/common/Log.java
@@ -1,7 +1,17 @@
 package com.tobsec.common;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-
-public @interface Log {
-
-}
+/**
+ * @Retention : 애노테이션 범위
+ * @Documented : 자바 문서에도 애노테이션 보임
+ * @Target : 애노테이션이 적용될 위치
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Documented
+public @interface Log {}
---
+++ src/main/java/com/tobsec/common/LogApprovalProcessor.java
@@ -0,0 +1,37 @@
+package com.tobsec.common;
+
+import java.lang.reflect.Field;
+
+import org.springframework.util.ReflectionUtils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class LogApprovalProcessor implements BeanPostProcessor {
+    
+    /**
+     * Bean 초기화 콜백 이전에 처리
+     */
+    public Object postProcessBeforeInitialization(final Object target, String beanName) throws BeansException {
+
+        ReflectionUtils.doWithLocalFields(
+            target.getClass()
+            , new ReflectionUtils.FieldCallback() {
+                @Override
+                @SuppressWarnings("unchecked")
+                public void doWith(Field field) throws IllegalArgumentException, IllegalAccessException {
+
+                }
+            }
+        );
+ 
+        return target;
+    }
+
+    /**
+     * Bean 초기화 콜백 이후에 반환(래핑할 때 사용하는 게 좋음)
+     */
+    public Object postProcessAfterInitialization(Object target, String beanName) throws BeansException {
+        return target;
+    }
+}
